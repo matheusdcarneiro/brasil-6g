063a6bccd502e7c385f13fab5989d3156dd939d2
matheusdomingoscarneiro <matheus.carneiro@gtel.ufc.br>
2024-12-03 15:19:38 -0300

    in progress

--- kpi.py
+++ kpi.py
@@ -37,7 +37,7 @@ def snr_sc(channel: float, power_vec: float, n_power: float):
 
     return snr_vector
 
-def snr_egc(channel: float, phases: float, power_vec: float, n_power: float):
+def snr_egc(channel: float, amp: float, power_vec: float, n_power: float):
     '''
     Returns the SNR vector after combining by same gain, in which the weight compensates
     the phase.
@@ -46,8 +46,8 @@ def snr_egc(channel: float, phases: float, power_vec: float, n_power: float):
     ----------
     channel : float
         Channel coefficient matrix [in linear] between an UE and the antennas of each AP.
-    phases: float
-        Complex exponential phase component between and UE.
+    amplitude: float
+        Channel amplitude component between and UE.
     power_vec : float
         Transmission power vector [in W] of each UE.
     n_power: float
@@ -63,7 +63,46 @@ def snr_egc(channel: float, phases: float, power_vec: float, n_power: float):
 
             # DO A AP-UE ASSOCIATION (IT IS NOT D-MIMO YET)!
 
-            weights = (phases[ue, ap, :])**(-1)
-            print(weights)
+            amplitude = amp[ue, ap, :]
+
+            snr_vector[ue] = ((((np.sum(amplitude))**2) / num_ant) * 
+                             (power_vec[ue] / n_power)**2)
+
+    return snr_vector
+
+def snr_mrc(channel: float, power_vec: float, n_power: float):
+    '''
+    Returns the SNR vector after combining by maximum ratio, in which the weight compensates de 
+    phase and maximize the amplitude.
+
+    Parameters
+    ----------
+    channel : float
+        Channel coefficient matrix [in linear] between an UE and the antennas of each AP.
+    power_vec : float
+        Transmission power vector [in W] of each UE.
+    n_power: float
+        Noise power [in W].
+    '''
+
+    num_ue, num_ap, num_ant = channel.shape 
+    snr_vector = np.zeros(num_ue)
+
+    for ue in range(num_ue):
+
+        for ap in range(num_ap):
+
+            # DO A AP-UE ASSOCIATION (IT IS NOT D-MIMO YET)!
+
+            snr_ant = np.zeros(num_ant)
+
+            for ant in range(num_ant):
+
+                # snr in each antenna
+                snr_ant[ant] = (np.absolute(channel[ue, ap, ant]) *
+                               (power_vec[ue] / n_power))**2   
+
+            # snr of each ue as the sum of snr antenna
+            snr_vector[ue] = np.sum(snr_ant)
 
     return snr_vector
--- main.py
+++ main.py
@@ -20,10 +20,12 @@ num_ap = 1
 num_ant = 4
 cov_side = 400
 total_bandwidth = 100e6
-seeds = 1
+seeds = 10000
 
 # Simulation data
 total_snr_sc = np.zeros((num_ue, seeds))
+total_snr_egc = np.zeros((num_ue, seeds))
+total_snr_mrc = np.zeros((num_ue, seeds))
 
 # Monte Carlo simulation
 for seed in range(seeds):
@@ -34,21 +36,32 @@ for seed in range(seeds):
     dis = distance(ue_pos, ap_pos)
 
     # Channel coefficient
-    channel, phases = generate_channel(dis, std_s, std_m, k, n, num_ue, num_ap, num_ant) 
-
-    print(channel.shape)
+    channel, phases, amp = generate_channel(dis, std_s, std_m, k, n, num_ue, num_ap, num_ant) 
 
     # SNR with combination by selection
     snr = snr_sc(channel, np.ones(num_ue), power_noise(total_bandwidth))
-    snr_ = snr_egc(channel, phases, np.ones(num_ue), power_noise(total_bandwidth))
+    snr_ = snr_egc(channel, amp, np.ones(num_ue), power_noise(total_bandwidth))
+    snr__ = snr_mrc(channel, np.ones(num_ue), power_noise(total_bandwidth))
 
     total_snr_sc[:, seed] = lin2db(snr)
+    total_snr_egc[:, seed] = lin2db(snr_)
+    total_snr_mrc[:, seed] = lin2db(snr__)
+
 
 # Plotting
-#snr_sc_ecdf = eCDF(total_snr_sc.flatten())
-#plt.plot(snr_sc_ecdf[0], snr_sc_ecdf[1])
+snr_sc_ecdf = eCDF(total_snr_sc.flatten())
+plt.plot(snr_sc_ecdf[0], snr_sc_ecdf[1], label='Selection combiner (SC)')
 
-#plt.xlabel('SNR [dB]')
-#plt.ylabel('eCDF')
-#plt.grid()
-#plt.show()
+snr_egc_ecdf = eCDF(total_snr_egc.flatten())
+plt.plot(snr_egc_ecdf[0], snr_egc_ecdf[1], label='Equal Gain Combiner (EGC)')
+
+snr_mrc_ecdf = eCDF(total_snr_mrc.flatten())
+plt.plot(snr_mrc_ecdf[0], snr_mrc_ecdf[1], label='Maximum Ration Combiner (MRC)')
+
+plt.xlabel('SNR [dB]')
+plt.ylabel('eCDF')
+plt.legend()
+plt.grid()
+plt.show()
+
+plt.save_fig('combiner_eCDF.pdf')
